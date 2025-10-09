5f19b2a544d0e441888609f9bc34750c1fb306b2
Marcus Edel <marcus.edel@collabora.com>
2025-10-09 15:45:38 -0400

    SET_ROWS and GET_ROWS has no TQ2_0 support yet.
    Signed-off-by: Marcus Edel <marcus.edel@collabora.com>

--- ggml/src/ggml-vulkan/ggml-vulkan.cpp
+++ ggml/src/ggml-vulkan/ggml-vulkan.cpp
@@ -12439,7 +12439,6 @@ static bool ggml_backend_vk_device_supports_op(ggml_backend_dev_t dev, const ggm
                     case GGML_TYPE_Q5_0:
                     case GGML_TYPE_Q5_1:
                     case GGML_TYPE_Q8_0:
-                    case GGML_TYPE_TQ2_0:
                     case GGML_TYPE_IQ1_S:
                     case GGML_TYPE_IQ1_M:
                     case GGML_TYPE_IQ2_XXS:
@@ -12466,7 +12465,6 @@ static bool ggml_backend_vk_device_supports_op(ggml_backend_dev_t dev, const ggm
                     case GGML_TYPE_Q5_0:
                     case GGML_TYPE_Q5_1:
                     case GGML_TYPE_Q8_0:
-                    case GGML_TYPE_TQ2_0:
                     case GGML_TYPE_IQ4_NL:
                         return true;
                     default:
